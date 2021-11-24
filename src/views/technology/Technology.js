b10b3bf27b9d194104e48b175a34e4fbc4febeea
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-11-24 08:47:34 -0700

    fix bad usestate import

--- src/views/technology/Technology.js
+++ src/views/technology/Technology.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useEffect, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import TechInfo from "./TechInfo";
 import TechSelector from "./TechSelector";
