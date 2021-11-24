fec06c7f6957613c2f33f4c539b15f7cf68bdf28
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-11-24 08:58:13 -0700

    fixing screen overflow mobile

--- src/views/crew/CrewInfo.js
+++ src/views/crew/CrewInfo.js
@@ -4,7 +4,7 @@ const CrewInfo = ({role, name, bio}) => {
       <div className="text-gray-500 uppercase font-light text-4xl text-center lg:text-justify">
         {role}
       </div>
-      <div className="text-gray-200  uppercase font-thin text-6xl lg:text-6xl text-center lg:text-justify">
+      <div className="text-gray-200  uppercase font-thin text-4xl lg:text-6xl text-center lg:text-justify">
         {name}
       </div>
       <div className="text-gray-400 max-w-md mx-auto lg:text-2xl lg:max-w-md lg:mx-0 text-center lg:text-justify">
